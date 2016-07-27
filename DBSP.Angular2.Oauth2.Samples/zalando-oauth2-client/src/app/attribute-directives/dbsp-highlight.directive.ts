fc34d3df0bc46421dff684273b58bce9a740f6ac
David1367 <pedro.571.david@gmail.com>
2016-07-27 12:44:56 -0400

    Finished with attribute directives

--- DBSP.Angular2.Oauth2.Samples/zalando-oauth2-client/src/app/attribute-directives/dbsp-highlight.directive.ts
+++ DBSP.Angular2.Oauth2.Samples/zalando-oauth2-client/src/app/attribute-directives/dbsp-highlight.directive.ts
@@ -1,8 +1,35 @@
-﻿import { Directive, ElementRef, Input }  from "@angular/core";
+﻿import { Directive, ElementRef, Input, HostListener }  from "@angular/core";
 
 @Directive({ selector: "[dbsp-highlight]" })
 export class DBSPHighlightDirective {
-  constructor(element: ElementRef) {
-    element.nativeElement.style.backgroundColor = "beige";
+  private _el: HTMLElement;
+  private _defaultColor: string = "red";
+
+  // highlightColor input has been overriden by the alias dbsp-highlight.
+  // If an alias is not used, the template should be written in the next way:
+  // <span dbsp-highlight [highlightColor]="'red'"></span>
+  // <h1 dbsp-highlight [highlightColor]="'blue'"></h1>
+  @Input("dbsp-highlight") highlightColor: string;
+
+  @Input() set defaultColor(color: string) {
+    this._defaultColor = color || this._defaultColor; 
+  } 
+
+  constructor(private element: ElementRef) {
+    this._el = element.nativeElement;
+  }
+
+  @HostListener("mouseenter")
+  onMouseEnter() {
+    this.highLight(this.highlightColor || this._defaultColor);
+  }
+
+  @HostListener("mouseleave")
+  onMouseLeave() {
+    this.highLight(null);
+  }
+
+  private highLight(color: string) {
+    this._el.style.backgroundColor = color;
   }
 }
