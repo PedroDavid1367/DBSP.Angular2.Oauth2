b677fd886521d2add1ec7b52aa97bd825ca5575c
David1367 <pedro.571.david@gmail.com>
2016-07-29 16:45:57 -0400

    Tested the use of a JS file

--- DBSP.Angular2.Oauth2.Samples/zalando-oauth2-client/src/app/app.component.ts
+++ DBSP.Angular2.Oauth2.Samples/zalando-oauth2-client/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component }               from '@angular/core';
-import { DBSPHighlightDirective }  from "./attribute-directives/dbsp-highlight.directive";
-import { DBSPNoteComponent }  from "./jquery-components/dbsp-note.component" 
+import { Component }                from '@angular/core';
+import { DBSPHighlightDirective }   from "./attribute-directives/dbsp-highlight.directive";
+import { DBSPNoteComponent }        from "./jquery-components/dbsp-note.component"
+import { DBSPRandomNoteComponent }  from "./jquery-components/dbsp-random-note.component" 
 
 @Component({
   selector: 'my-app',
   template: require('./app.component.html'),
-  directives: [DBSPHighlightDirective, DBSPNoteComponent],
+  directives: [DBSPHighlightDirective, DBSPNoteComponent, DBSPRandomNoteComponent],
   styles: [require('./app.component.css')]
 })
 export class AppComponent { }
---
+++ DBSP.Angular2.Oauth2.Samples/zalando-oauth2-client/src/app/jquery-components/dbsp-random-note.component.ts
@@ -0,0 +1,16 @@
+﻿let sample = require("./sample.js");
+
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "dbsp-random-note",
+  template: `
+  <hr/>
+  Hello
+  `
+})
+export class DBSPRandomNoteComponent {
+  constructor() {
+    sample();
+  }
+}
