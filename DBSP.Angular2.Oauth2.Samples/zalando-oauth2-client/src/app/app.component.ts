d21f1960ec2745ed24c211390bc990fce2dda4c8
David1367 <pedro.571.david@gmail.com>
2016-08-01 14:19:46 -0400

    added some minor component

--- DBSP.Angular2.Oauth2.Samples/zalando-oauth2-client/src/app/app.component.ts
+++ DBSP.Angular2.Oauth2.Samples/zalando-oauth2-client/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component }                from '@angular/core';
 import { DBSPHighlightDirective }   from "./attribute-directives/dbsp-highlight.directive";
 import { DBSPNoteComponent }        from "./jquery-components/dbsp-note.component"
-import { DBSPRandomNoteComponent }  from "./jquery-components/dbsp-random-note.component" 
+import { DBSPRandomNoteComponent }  from "./jquery-components/dbsp-random-note.component"
+import { DBSPStylingParentComponent }        from "./component-styles/dbsp-styling-parent.component"
 
 @Component({
   selector: 'my-app',
   template: require('./app.component.html'),
-  directives: [DBSPHighlightDirective, DBSPNoteComponent, DBSPRandomNoteComponent],
+  directives: [DBSPHighlightDirective, DBSPNoteComponent,
+    DBSPRandomNoteComponent, DBSPStylingParentComponent],
   styles: [require('./app.component.css')]
 })
 export class AppComponent { }
---
+++ DBSP.Angular2.Oauth2.Samples/zalando-oauth2-client/src/app/component-styles/dbsp-styling-parent.component.ts
@@ -0,0 +1,24 @@
+﻿import { Component }  from "@angular/core";
+
+@Component({
+  selector: "dbsp-styling-parent",
+  styleUrls: ["./src/app/component-styles/style-a.css"],
+  template: `
+  <div>
+    Some text
+    <div>
+      More text
+      <div>
+        Even more text
+      </div>
+    </div>
+  </div>
+  <span>I'm a span</span>
+  <br />
+  <p>I'm a paragraph</p>
+  <span class="active">Another span</span>
+  <label>I'm a label</label>
+  `
+})
+export class DBSPStylingParentComponent {
+}
